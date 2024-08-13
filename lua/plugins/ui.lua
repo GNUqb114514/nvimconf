63e35e9f24f8d809779e128b8b408e9fd028a3b4
GNUqb114514 <GNUqb114514@outlook.com>
2024-08-13 15:58:31 +0800

    Split plugin specs into separated file.
        Also add lualine.

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,20 @@
+return {
+    {   -- Colorscheme: BEFORE ALL OTHERS
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function ()
+            vim.cmd.colorscheme'tokyonight-night'
+        end
+    },
+    {   -- Status line: Start at starting UI
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        lazy = false,
+        opts = {
+            options = {
+                theme = 'tokyonight',
+            },
+        },
+    },
+}
