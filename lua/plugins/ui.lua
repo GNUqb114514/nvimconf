e5a2a1e1385ec32ca3acb1e54df6be6a7924c5c2
GNUqb114514 <GNUqb114514@outlook.com>
2024-08-13 16:14:03 +0800

    Remove 'options.theme' for lualine.
        Because it's not required.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,11 +11,7 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         lazy = false,
-        opts = {
-            options = {
-                theme = 'tokyonight',
-            },
-        },
+        opts = {},
     },
     {
         'utilyre/barbecue.nvim',
