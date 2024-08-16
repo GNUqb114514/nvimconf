ce8f4a41954ed9f51189305cfb342f6c79bb4a37
GNUqb114514 <GNUqb114514@outlook.com>
2024-08-16 15:25:37 +0800

    Add telescope key bindings.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,12 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         'nvim-tree/nvim-web-devicons',
     },
-    opts = {},
+    config = function ()
+        require('telescope').load_extension('fzf')
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>tw', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+    end
 }
