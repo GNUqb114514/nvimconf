1741f6d6629539875e538fea3eb9e8ae4890a0b5
GNUqb114514 <GNUqb114514@outlook.com>
2024-08-23 17:54:03 +0800

    Add snippets provided by luasnip.
        Configs counsulted https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,5 +1,8 @@
 return {
     {
+        "L3MON4D3/LuaSnip",
+    },
+    {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-path",
@@ -7,14 +10,27 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-cmdline",
             "hrsh7th/cmp-buffer",
+            {
+                "saadparwaiz1/cmp_luasnip",
+                dependencies = {
+                    "L3MON4D3/LuaSnip",
+                }
+            },
         },
         config = function ()
             local cmp = require('cmp')
+            local luasnip = require('luasnip')
             cmp.setup {
+                snippet = {
+                    expand = function (args)
+                        require('luasnip').lsp_expand(args.body)
+                    end
+                },
                 sources = {
                     {name = "path"},
                     {name = "calc"},
                     {name = "nvim_lsp"},
+                    { name = "luasnip" },
                 },
                 window = {
                     documentation = cmp.config.window.bordered(),
@@ -23,6 +39,19 @@ return {
                     ['<Tab>'] = function (fallback)
                         if cmp.visible() then
                             cmp.select_next_item{behavior=cmp.SelectBehavior.Insert}
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end,
+                    ['<CR>'] = function (fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({select = false})
+                            end
                         else
                             fallback()
                         end
@@ -30,6 +59,8 @@ return {
                     ['<S-Tab>'] = function (fallback)
                         if cmp.visible() then
                             cmp.select_prev_item{behavior=cmp.SelectBehavior.Insert}
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
                         else
                             fallback()
                         end
