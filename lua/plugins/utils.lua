7ad7137980d6e734f4f2387971ed6b80005d18ee
GNUqb114514 <GNUqb114514@outlook.com>
2024-08-25 10:10:18 +0800

    Disable flash.nvim.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -14,6 +14,7 @@ return {
             { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
+        enable=false,
     },
     {
         "nvim-neo-tree/neo-tree.nvim",
